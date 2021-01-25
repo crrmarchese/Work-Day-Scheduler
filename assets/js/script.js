0cc28de16f8c3b69f00649cc8b3016d6d8da6fd2
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-01-24 21:38:03 -0500

    Edit getCurrentHour function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ const currentMonth = (dayjs().format("MMMM"));
 const dayOfWeek = (dayjs().format("dddd"));
 const dayOfMonth = (dayjs().format("D"));
 const timeOfDay = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-const computerTimeOfDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// Loop through hours in day to get an array from 9-17
+let computerTimeOfDay = [];
+ // Get current hour from computer
+let currentHourNow = parseInt((dayjs().get('hour')));
+//Set variables for tables structure
 const rowItem$ = $("<div class='row time-block'>");
 const colItem$ = $("<div class='col-12 table-responsive'>");
 const renderTable$ = $("<table class='table'>");
@@ -13,6 +17,11 @@ const tableTr$ = $("<tr class='tr-row'>");
 // const textAreaInput = $("<textarea class='description'>");
 const storageKey = "myCalendar";
 
+
+// Array of hours in day 9-17
+for (let i = 9; i <= 17; i++) {
+    computerTimeOfDay.push(i);    
+}
 
 // Append the <div.row> to <div.container>
 $(".container").append(rowItem$);
@@ -29,10 +38,10 @@ $(colItem$).append(renderTable$);
 $("#currentDay").text(dayOfWeek + ", "+ currentMonth + " " + dayOfMonth);
 
 let output = "";
-for(let i = 8; i < timeOfDay.length; i++) {
+for(let i = 0; i < timeOfDay.length; i++) {
     // output html elements
     // console.log(timeOfDay[i]);
-    output = timeOfDay[i];
+    output = timeOfDay[i]; // 9AM, 10AM etc
     
 
     // console.log(output);
@@ -41,7 +50,7 @@ for(let i = 8; i < timeOfDay.length; i++) {
     $(renderTableBody$).append(tableTr$);
 
     // Return the time inside first <td>
-    const timeTd$ = $("<td class='hour time-of-day'>").text(output).attr("data-time", computerTimeOfDay[i]);;
+    const timeTd$ = $("<td class='hour time-of-day'>").text(output).attr("data-time", computerTimeOfDay[i]); // 9-17
 
     // Create <textarea> inside second <td>
     const textareaTd$ = $("<td class='description-details'>").html("<textarea class='description'>");
@@ -51,38 +60,34 @@ for(let i = 8; i < timeOfDay.length; i++) {
 
     // Append the 3 <td>s inside a <tr>
     $(tableTr$).append(timeTd$, textareaTd$, saveBtnTd$);
-    
+   
 
 }
  // Append the <tbody> inside <table>
  $(renderTable$).append(renderTableBody$);
+ getCurrentHour();
 
  // Get current hour from computer
  function getCurrentHour() {
-    let currentHourNow = (dayjs().get('hour')); 
-    
-    //   for(let i = 0; i < computerTimeOfDay.length; i++) {
-
-        // Get data-time attribute and convert to integer
-        let timeDataAttr = $("td.hour").attr("data-time"); 
-        let timeDataAttrNum = parseInt(timeDataAttr);
-    // console.log(currentHourNow);
    
+        // Get data-time attribute which is computerTimeOfDay
+        let timeDataAttr = parseInt($("td.hour").attr("data-time")); 
+    //    console.log(timeDataAttr);
         // Compare if data-time attribute matches the current hour set by computer
         /* Return appropriate class depending if the hour is before, current or after
          current hour */
-        if(timeDataAttrNum === currentHourNow) {
+        if(timeDataAttr === currentHourNow) {
             $(".description-details").addClass("present");
         }
-        if(timeDataAttrNum < currentHourNow) {
+        if(timeDataAttr < currentHourNow) {
             $(".description-details").addClass("past");
         }
-        if (timeDataAttrNum > currentHourNow){
+        if (timeDataAttr > currentHourNow){
             $(".description-details").addClass("future");
         }
-    //   }
+    
  }
-// getCurrentHour();
+//  getCurrentHour();
 
 // Local Storage
 function saveToLocalStorage() {
@@ -109,4 +114,3 @@ function saveToLocalStorage() {
 }
 
 saveToLocalStorage();
-
